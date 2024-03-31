e133e93c0d5688997b2111055c95fa2d365ea8d2
Jessica Été <kohrVid@tutanota.com>
2024-03-31 15:56:59 +0200

    Configure basic environment settings
    The dev environment has been configured to use port 1234 by default (as
    I expect the rails API to run on port 3000).
    
    I've also added a basic template for the environments file. Anyone who
    runs this app will either have to add the missing variable to this file
    or create a new `.env*.local` file that includes these environment
    variables. The rewritten endpoints used in the frontend's proxy (for
    requests made to the API) depends on the environment variables set in
    these files.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const env = process.env.ENV;
+const apiDomain = process.env.API_DOMAIN;
+
+const rewriteEndpoints = [
+  {
+    source: '/api/:path*',
+    destination: `${apiDomain}/:path*`,
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return (env.toLowerCase() !== 'production') ? (
+      rewriteEndpoints
+    ) : (
+       []
+    );
+  },
 };
 
 export default nextConfig;
