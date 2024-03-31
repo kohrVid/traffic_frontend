4c95c569629f6d20df7b9c0182ba3cc78edb57e0
Jessica Été <kohrVid@tutanota.com>
2024-04-01 01:48:31 +0200

    Remove default NextJS styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="content-language" content="en-GB" />
+        <meta name="description" content="Page Traffic website" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>traffic</title>
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
