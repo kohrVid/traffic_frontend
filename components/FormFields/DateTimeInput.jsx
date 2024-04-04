c8375eab034cd4f747c15a9079e43372660bc14d
Jessica Été <kohrVid@tutanota.com>
2024-04-04 17:39:25 +0200

    Add support for onChange events to FormField inputs

--- components/FormFields/DateTimeInput.jsx
+++ components/FormFields/DateTimeInput.jsx
@@ -1,10 +1,10 @@
 import styles from './styles.module.scss';
 
-export const DateTimeInput = ({ name }) => {
+export const DateTimeInput = ({ name, onChange }) => {
   return (
     <div className={styles.date}>
       <label htmlFor={name}>{name}:</label>
-      <input type="datetime-local" id={name} name={name} />
+      <input type="datetime-local" id={name} name={name} onChange={onChange} />
     </div>
   );
 }
--- components/FormFields/SelectInput.jsx
+++ components/FormFields/SelectInput.jsx
@@ -1,11 +1,11 @@
 import styles from './styles.module.scss';
 
-export const SelectInput = ({ name, options }) => {
+export const SelectInput = ({ name, options, onChange }) => {
   return (
     <div>
       <label htmlFor={name}>Select a page:</label>
 
-      <select name={name} id={name}>
+      <select name={name} id={name} onChange={onChange}>
         {options.map((option, idx) => (
           <option key={`${name}-option-${idx}`} value={option.id}>
             {option.name}
